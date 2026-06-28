40fbb23f69681db7c5ddca636230003f7fdc1ffb
dbaylan3301 <dbaylan3301@gmail.com>
2026-06-28 17:49:58 +0300

    feat: update AI module exports

--- plugins/scalpbot/ai/__init__.py
+++ plugins/scalpbot/ai/__init__.py
@@ -1,6 +1,31 @@
 """AI Brain modules for ScalpBot."""
+
 try:
     from .brain import MetaBrain
-    __all__ = ["MetaBrain"]
 except ImportError:
-    __all__ = []
+    MetaBrain = None
+
+try:
+    from .neural.scalp_brain import ScalpBrain
+except ImportError:
+    ScalpBrain = None
+
+try:
+    from .hybrid.neuro_symbolic import NeuroSymbolicTrader
+except ImportError:
+    NeuroSymbolicTrader = None
+
+try:
+    from .symbolic.rule_engine import SymbolicRuleEngine
+except ImportError:
+    SymbolicRuleEngine = None
+
+try:
+    from .forge.ai_forge import AIForge
+except ImportError:
+    AIForge = None
+
+__all__ = [
+    "MetaBrain", "ScalpBrain", "NeuroSymbolicTrader",
+    "SymbolicRuleEngine", "AIForge"
+]
